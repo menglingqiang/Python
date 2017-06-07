d7cfbcbbc7d88bc9b582aff5e53286e20b890acc
menglingqiang <1820026438@qq.com>
2017-06-07 23:25:32 +0800

    统计不同语种的歌曲个数

--- test/test.py
+++ test/test.py
@@ -1,11 +1,14 @@
 # coding=UTF-8
 import codecs
 import json
+import re
+import urllib.request
+
 from  bs4 import BeautifulSoup
 import bs4
 from pip._vendor.appdirs import unicode
 import requests
-import urllib.request
+
 
 def downloadHtml(url):
     try:
@@ -22,43 +25,61 @@ def parse(html,ulist):
         if(isinstance(tr, bs4.element.Tag)):
             tds = tr('td')
             ulist.append([tds[0].string])
+def check_contain_chinese(check_str):
+    for ch in check_str:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+    return False
 def main():
-#     id = input("请输入您想要获取的云音乐列表 id:\n")
-#     tplt = "\t{:80}\t{:80}\t{:80}"
+    id = input("请输入您想要获取的云音乐列表 id:\n")
+    tplt = "\t{:80}\t{:80}\t{:80}"
 #     print(tplt.format("歌曲", "歌手", "专辑"))
-#     id=""
-#     url = 'http://music.163.com/playlist?id='+id
-#     html = downloadHtml(url)
-#     soup = BeautifulSoup(html,'html.parser')
-#     summary_node = soup.find('textarea') #得到所有歌曲的json串
-#     musicJson = summary_node.get_text()
-#     data = json.loads(musicJson)
-#     count = len(data)
+    url = 'http://music.163.com/playlist?id='+id
+    html = downloadHtml(url)
+    soup = BeautifulSoup(html,'html.parser')
+    summary_node = soup.find('textarea') #得到所有歌曲的json串
+    musicJson = summary_node.get_text()
+    data = json.loads(musicJson)
+    count = len(data)
 #     for i in range(count):
 #         print(tplt.format(data[i]['name'],data[i]['artists'][0]['name'],data[i]['album']['name']))
-    f = urllib.request.urlopen('http://m10.music.126.net/20170604134656/4d5a8e553045cad820afcc665b0c92f7/ymusic/9a1b/954a/4b1a/5e7c012e7a6e44690794486d1be90531.mp3')    
-    data = f.read()    
-    with open('C:\\Users\\mlq\\Desktop\\downmusic\\download.mp3', 'wb') as code:    
-        code.write(data)  
-    #data = json.loads(html)
-    #print(data)
-    #file_object = open('test.txt','wb')
-    #with open('test.txt', 'w',encoding="utf-8") as f:
-    #    f.write(tplt.format("歌曲", "歌手", "专辑")+'\n')
-    #    for i in range(count):
-        #    str = unicode(tplt.format(data[i]['name'],data[i]['artists'][0]['name'],data[i]['album']['name']),"utf-8")
-    #        f.write(tplt.format(data[i]['name'],data[i]['artists'][0]['name'],data[i]['album']['name'])+'\n')
-    #    f.close( )
-    #    all_the_text = file_object.read( ).decode('utf-8')
-    #    print(all_the_text)
-    #finally:
-    #     file_object.close( )
-    #with open('test.txt', 'rb') as f:
-    #    data = json.load(f)
-main()    
+#     f = urllib.request.urlopen('http://m10.music.126.net/20170604134656/4d5a8e553045cad820afcc665b0c92f7/ymusic/9a1b/954a/4b1a/5e7c012e7a6e44690794486d1be90531.mp3')    
+#     data = f.read()    
+#     with open('C:\\Users\\mlq\\Desktop\\downmusic\\download.mp3', 'wb') as code:    
+#         code.write(data)  
+#     data = json.loads(html)
+#     print(data)
+    cMusic =0  #中文歌曲数量
+    eMusic =0  #英文歌曲数量
+    oMusic =0  #其他国家歌曲数量
     
-#+"歌手:"
-#       +data[i]['artists']['name']+"专辑:"+data[i]['album']['name']   
+    ePattern = re.compile(r'[a-zA-Z\.()0-9]+')  #英文正则,全是英文，下滑线，括号，空格就是英文  
+#    file_object = open('test.txt','wb')
+    with open('test.txt', 'w',encoding="utf-8") as f:
+        f.write(tplt.format("歌曲", "歌手", "专辑")+'\n')
+        for i in range(count):
+            #cMatch = cPattern.match(data[i]['name'])
+            eMatch = ePattern.match(data[i]['name'])
+            
+            if(check_contain_chinese(data[i]['name'])):    
+                cMusic +=1
+            elif(eMatch):
+                print(data[i]['name'])
+                eMusic +=1
+            else:
+                oMusic +=1   
+            #str = unicode(tplt.format(data[i]['name'],data[i]['artists'][0]['name'],data[i]['album']['name']),"utf-8")
+            f.write(tplt.format(data[i]['name'],data[i]['artists'][0]['name'],data[i]['album']['name'])+'\n')
+        f.write('中文歌曲：'+str(cMusic)+'\n'+'英文歌曲：'+str(eMusic)+'\n'+'其他国家歌曲：'+str(oMusic))
+        f.close()
+#        all_the_text = file_object.read( ).decode('utf-8')
+#        print(all_the_text)
+#     finally:
+#        file_object.close()
+    #每次读取一行判断是否中文，英文，其他语言并进行总结
+    
+main()    
+      
     
     
     
